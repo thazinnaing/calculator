e37b9f3d501a91fef367832bcfda7ef138e1e6d3
thazin naing <thazinaingmm@gmail.com>
2023-07-21 20:59:46 +0630

    first commit, not responsive

---
+++ src/components/digitButton.jsx
@@ -0,0 +1,18 @@
+import { ACTIONS } from "../App"
+import "../App.css"
+
+
+const DigitButton=({digit,dispatch})=>{
+    return(
+        <>
+        <button onClick={()=>{
+            dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})
+        }}
+        >
+            {digit}
+        </button>
+        </>
+    )
+}
+
+export default DigitButton;
---
+++ src/components/operationButton.jsx
@@ -0,0 +1,20 @@
+import { ACTIONS } from "../App";
+import "../App.css"
+
+
+const OperationButton=({operation,dispatch})=>{
+    return(
+        <>
+        <button onClick={()=>{
+            dispatch({
+                type: ACTIONS.CHOOSE_OPERATION,
+                payload: {operation}
+            })
+        }}>
+            {operation}
+        </button>
+        </>
+    )
+}
+
+export default OperationButton;
